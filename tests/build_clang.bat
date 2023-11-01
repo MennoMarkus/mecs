231331903758ecbce4e293bdd1292c6bb7decc45
Menno Markus <45625943+MennoMarkus@users.noreply.github.com>
2023-11-01 20:06:01 +0000

    Adding C++ support for portable alignof.

--- tests/build_clang.bat
+++ tests/build_clang.bat
@@ -1,17 +1,19 @@
 
 @echo off
 
-REM usage: build.bat [config]
+REM usage: build.bat [config] [languageVersion]
 REM config - { debug, release, preprocessor }, default=debug
+REM languageVersion - { c, cpp }, default=c
 
 REM 1) Setup configuration
 set config=%1
+set languageVersion=%2
 if "%config%" == "" set config="debug"
+if "%languageVersion%" == "" set languageVersion="c"
 set rootFolder=%cd%
 set source=%cd%/main.c
 set outputName=%cd%/output/main_clang.exe
 
-REM -std=c89                - set C version to C89
 REM -Wall                   - enable all warnings
 REM -Werror                 - turn warnings into errors
 REM -pedantic-errors        - error on language extensions 
@@ -22,15 +24,22 @@ REM -gcodeview              - generate code view debug information for visual st
 REM -I                      - specify include directory
 REM -o                      - specify output executable name
 REM -E                      - ouput the result form the preprocessor to a file
-set debugOpts=-std=c89 -Wall -Werror -pedantic-errors -O3 -g -gcodeview -I"%rootFolder%" -o%outputName%
-set releaseOpts=-std=c89 -Wall -Werror -pedantic-errors -Ofast -I"%rootFolder%" -o%outputName%
+set debugOpts=-Wall -Werror -pedantic-errors -O3 -g -gcodeview -I"%rootFolder%" -o%outputName%
+set releaseOpts=-Wall -Werror -pedantic-errors -Ofast -I"%rootFolder%" -o%outputName%
 
 set opts=%debugOpts%
 if %config% == "preprocessor" set opts=%debugOpts% -E
 if %config% == "release" set opts=%releaseOpts%
 
-REM 2) Create output directory
+REM 2) Set the language version
+REM -std=c89                - set C version to C89
+REM -std=c++98              - set C++ version to C++98/C++03
+REM -x                      - compile C files as C++
+if %languageVersion% == "c" set opts=-std=c89 %opts%
+if %languageVersion% == "cpp" set opts=-std=c++98 -x c++ %opts%
+
+REM 3) Create output directory
 if not exist output mkdir output
 
-REM 3) Compile executable
+REM 4) Compile executable
 call clang %opts% %source% 
